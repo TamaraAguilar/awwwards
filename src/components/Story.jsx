81eb5185cb1bb9d1602273fc2618387b978a9eaa
TamaraAguilar <tamaraaguilar5@gmail.com>
2026-04-25 21:23:45 -0400

    feat: create story section

---
+++ src/components/Story.jsx
@@ -0,0 +1,40 @@
+import React, { useRef } from "react";
+import AnimatedTitle from "./AnimatedTitle";
+
+function Story() {
+  const frameRef = useRef("null");
+
+  const handleMouseLeave = () => {};
+
+  return (
+    <section id="story" className="min-h-dvh w-screen bg-black text-blue-50">
+      <div className="flex size-full flex-col items-center py-10 pb-24">
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          the multiversal ip world
+        </p>
+        <div className="relative size-full">
+          <AnimatedTitle
+            title="The st<b>o</b>ry of <br/> a hidden real<b>m</b>"
+            sectionId="#story"
+            containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
+          />
+          <div className="story-img-container">
+            <div className="story-img-mask">
+              <div className="story-img-content">
+                <img
+                  src="/img/entrance.webp"
+                  alt="Entrance"
+                  className="object-contain"
+                  ref={frameRef}
+                  onMouseLeave={handleMouseLeave}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Story;
