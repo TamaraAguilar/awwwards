7d6be200b047932702fc0e1cc62f3c18f8216bba
TamaraAguilar <tamaraaguilar5@gmail.com>
2026-04-25 21:28:17 -0400

    feat: add handleMouseLeave and handleMouseMove

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -2,9 +2,45 @@ import React, { useRef } from "react";
 import AnimatedTitle from "./AnimatedTitle";
 
 function Story() {
-  const frameRef = useRef("null");
+  const frameRef = useRef(null);
 
-  const handleMouseLeave = () => {};
+  const handleMouseLeave = () => {
+    const element = frameRef.current;
+
+    if (element) {
+      gsap.to(element, {
+        duration: 0.3,
+        rotateX: 0,
+        rotateY: 0,
+        ease: "power1.inOut",
+      });
+    }
+  };
+
+  const handleMouseMove = (e) => {
+    const { clientX, clientY } = e;
+    const element = frameRef.current;
+
+    if (!element) return;
+
+    const rect = element.getBoundingClientRect();
+    const xPos = clientX - rect.left;
+    const yPos = clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = ((yPos - centerY) / centerY) * -10;
+    const rotateY = ((xPos - centerX) / centerX) * 10;
+
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX,
+      rotateY,
+      transformPerspective: 500,
+      ease: "power1.inOut",
+    });
+  };
 
   return (
     <section id="story" className="min-h-dvh w-screen bg-black text-blue-50">
@@ -22,14 +58,44 @@ function Story() {
             <div className="story-img-mask">
               <div className="story-img-content">
                 <img
-                  src="/img/entrance.webp"
-                  alt="Entrance"
-                  className="object-contain"
                   ref={frameRef}
+                  onMouseMove={handleMouseMove}
                   onMouseLeave={handleMouseLeave}
+                  onMouseUp={handleMouseLeave}
+                  onMouseEnter={handleMouseLeave}
+                  src="/img/entrance.webp"
+                  alt="entrance.webp"
+                  className="object-contain"
                 />
               </div>
             </div>
+
+            {/* for the rounded corner */}
+            <svg
+              className="invisible absolute size-0"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <defs>
+                <filter id="flt_tag">
+                  <feGaussianBlur
+                    in="SourceGraphic"
+                    stdDeviation="8"
+                    result="blur"
+                  />
+                  <feColorMatrix
+                    in="blur"
+                    mode="matrix"
+                    values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+                    result="flt_tag"
+                  />
+                  <feComposite
+                    in="SourceGraphic"
+                    in2="flt_tag"
+                    operator="atop"
+                  />
+                </filter>
+              </defs>
+            </svg>
           </div>
         </div>
       </div>
